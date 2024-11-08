3c84667c7978c1520c1a1b2d47a82a10547bf769
Sebastián Maldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2024-11-08 17:13:18 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -350,7 +350,24 @@ function build_m_table(productions, nexts) {
 app.use(express.json());
 
 // API endpoint that receives text, executes the JS function, and returns a dictionary
-app.post('/execute', (req, res) => {
+app.post('/execute', (req, res, next) => {
+    res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+    
     try {
         const { text } = req.body; // Receive the text (JavaScript function)
         const grammar = read_grammar(text);
