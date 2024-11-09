136f6ed79d6fd282ca27a4b08ca2c0ac86e88d10
Sebastián Maldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2024-11-09 10:51:12 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -427,7 +427,7 @@ function recognize_string(mTable, inputString) {
         } else if (mTable[top] && mTable[top][currentInput]) {
             // If top is a non-terminal and we have a rule in M table
             const production = mTable[top][currentInput];
-       // Check if production is defined
+            // Check if production is defined
             if (!production) {
                 trace[trace.length - 1].action = `Error: No production rule found for "${top}" with input "${currentInput}"`;
                 return { result: false, trace }; // Return failure with trace
@@ -459,55 +459,7 @@ function recognize_string(mTable, inputString) {
         action: "Error: Stack is empty but input is not fully parsed."
     });
     return { result: false, trace }; // Return failure with trace
-        }
-
-// Middleware to parse incoming JSON data
-app.use(express.json());
-
-// API endpoint that receives text, executes the JS function, and returns a dictionary
-app.post('/execute', (req, res, next) => {
-    res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-    
-    try {
-        const { text } = req.body; // Receive the text (JavaScript function)
-        const {grammar, non_recursive, all_firsts, all_nexts, m_table} = syntax_analysis(text)
-
-// Testing the outputs of the syntax analysis function 
-
-console.log("...............")
-console.log(all_firsts)
-console.log(all_nexts)
-console.log(m_table)
-
-    
-    
-
-        // Return the result in a dictionary (JSON object)
-        res.json({ success: true, result: {tabla_m: m_table},
-                  siguientes: all_nexts, primeros: all_firsts,
-                 text: non_recursive});
-    } catch (err) {
-        res.status(400).json({ success: false, error: err.message });
-    }
-
-    
-
-});
+}
 
 app.post('/recognize', (req, res, next) => {
     res.setHeader(
